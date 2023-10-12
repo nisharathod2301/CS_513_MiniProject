919a2aec5b1f5fd9b46c6745b62e060c26893a43
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2023-10-12 16:05:02 +0530

    recieves roles and processes them

--- ServerSide.c
+++ ServerSide.c
@@ -2,124 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
-struct User {
-    char username[50];
-    char password[50];
+
+
+// Function to send prompts and messages to the client
+// ... Other code ...
+
+// Function to send prompts and messages to the client
+void sendPrompts(int client) {
+    char welcomeMessage[] = "...Welcome Back to Academia :: Course Registration...\nLogin Type \n Enter Your Choice (1.Admin, 2.Professor, 3. Student):";
+    send(client, welcomeMessage, sizeof(welcomeMessage), 0);
+
+    // Wait for an acknowledgment from the client before sending the second message
+    char acknowledgment[256];
+    recv(client, acknowledgment, sizeof(acknowledgment), 0);
+
+}
+
+// Function to handle a client connection
+void* handleClient(void* clientSocket) {
+    int client = *((int*)clientSocket);
+    sendPrompts(client);
+    
     int role;
-    int login_id;
-};
+    recv(client, &role, sizeof(role), 0);
 
-// Function to authenticate the user using user data from file
-int authenticate_user(const char* username, const char* password, int role, int* login_id) {
-    FILE* file = fopen("user_data.txt", "r");
-    if (file == NULL) {
-        perror("Failed to open user_data.txt");
-        exit(1);
+    // Send an acknowledgment to the client
+    char acknowledgment[256];
+    send(client, acknowledgment, sizeof(acknowledgment), 0);
+    
+    if (role >= 1 && role <= 3) {
+            char usernameMessage[] = "Enter username: ";
+            send(client, usernameMessage, sizeof(usernameMessage), 0);
     }
-
-    char stored_username[50], stored_password[50];
-    int stored_role, stored_login_id;
-
-    while (fscanf(file, "%s %s %d %d", stored_username, stored_password, &stored_role, &stored_login_id) != EOF) {
-        if (strcmp(username, stored_username) == 0 && strcmp(password, stored_password) == 0 && role == stored_role) {
-            *login_id = stored_login_id;
-            fclose(file);
-            return 1; // Authentication successful
-        }
+    else{
+        char errormessage[] = "Enter correct role ID!";
+        send(client, errormessage, sizeof(errormessage),0);
     }
-
-    fclose(file);
-    return 0; // Authentication failed
+    return NULL;
 }
 
 int main() {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t addr_size;
+    pthread_t thread_id;
 
-    // Create server socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        perror("Socket creation failed");
-        exit(1);
-    }
+    // Create a socket
+    serverSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-    // Initialize server address struct
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080); // Port number
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    // Configure the server address
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind the server socket to the specified address and port
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Socket binding failed");
-        exit(1);
-    }
+    // Bind the socket to the address
+    bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 
     // Listen for incoming connections
-    if (listen(server_socket, 5) == -1) {
-        perror("Listening failed");
-        exit(1);
-    }
+    listen(serverSocket, 10);
 
     printf("Server is listening...\n");
 
     while (1) {
-        // Accept incoming client connection
-        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
-        if (client_socket == -1) {
-            perror("Connection accept failed");
-            continue;
+        addr_size = sizeof(clientAddr);
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addr_size);
+        int* newClient = (int*)malloc(1);
+        *newClient = clientSocket;
+
+        // Create a new thread to handle the client
+        if (pthread_create(&thread_id, NULL, handleClient, (void*)newClient) != 0) {
+            perror("Error creating thread");
+            return 1;
         }
-
-        int role;
-        int login_id;
-        char username[50];
-        char password[50];
-
-        // Receive the role from the client
-        recv(client_socket, &role, sizeof(int), 0);
-
-        // Receive username and password from the client
-        recv(client_socket, username, sizeof(username), 0);
-        recv(client_socket, password, sizeof(password), 0);
-
-        // Authenticate the user with the received role
-        if (authenticate_user(username, password, role, &login_id)) {
-            // Send the role back to the client
-            send(client_socket, &role, sizeof(int), 0);
-
-            // Redirect based on the role
-            switch (role) {
-                case 1:
-                    // Admin module
-                    printf("Admin with Login-ID: MT%d logged in.\n", login_id);
-                    break;
-                case 2:
-                    // Professor module
-                    printf("Professor with Login-ID: MT%d logged in.\n", login_id);
-                    break;
-                case 3:
-                    // Student module
-                    printf("Student with Login-ID: MT%d logged in.\n", login_id);
-                    break;
-                default:
-                    printf("Unknown role for user '%s'.\n", username);
-            }
-        } else {
-            // Authentication failed
-            role = -1;
-            send(client_socket, &role, sizeof(int), 0);
-            printf("Authentication failed. Please check your username, password, and role.\n");
-        }
-        // Close the client socket
-        close(client_socket);
     }
-
-    // Close the server socket (this part may not be reached in practice)
-    close(server_socket);
 
     return 0;
 }
