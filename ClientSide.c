28f2cc3d92f54cdd93829b54625051ab17eb9343
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2023-10-12 16:04:26 +0530

    added robust acknowledgement sys

--- ClientSide.c
+++ ClientSide.c
@@ -3,91 +3,59 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
-
-// Function to handle the admin menu (add_student)
-void add_student(int client_socket) {
-    // Implement add_student functionality here
-}
+#include <sys/socket.h>
 
 int main() {
-    int client_socket;
-    struct sockaddr_in server_addr;
+    int clientSocket;
+    struct sockaddr_in serverAddr;
 
-    // Create client socket
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("Socket creation failed");
-        exit(1);
-    }
+    // Create a socket
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-    // Initialize server address struct
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080); // Port number
-    server_addr.sin_addr.s_addr = inet_addr("172.16.144.112"); // Server IP address
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080); // Port number
+    serverAddr.sin_addr.s_addr = inet_addr("172.16.144.112"); // Server IP address
 
     // Connect to the server
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Connection to server failed");
-        exit(1);
-    }
+    connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    // Receive and display prompts from the server
+    char buffer[256];
+    memset(buffer, 0, sizeof(buffer));
 
+    // Receive welocme wala
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    printf("%s", buffer);
+
+    // Send an acknowledgment to the server
+    char acknowledgment[] = "Received";
+    send(clientSocket, acknowledgment, sizeof(acknowledgment), 0);
+
+    // Wait for 1 second
+    sleep(1);
+
+    memset(buffer, 0, sizeof(buffer));  // Clear the buffer
+
+// Prompt the user to enter an integer
     int role;
-
-    // Prompt the user for their role
-    printf("Welcome Back to Academia :: Course Registration\n");
-    printf("Login Type\n");
-    printf("Enter Your Choice (1.Admin, 2.Professor, 3. Student): ");
     scanf("%d", &role);
-    send(client_socket, &role, sizeof(int), 0);
 
-    char username[50];
-    char password[50];
+    // Send the integer to the server
+    send(clientSocket, &role, sizeof(role), 0);
 
-    // Prompt the user for their username and password
-    printf("Enter username: ");
-    scanf("%s", username);
-    printf("Enter Password: ");
-    scanf("%s", password);
+    // Receive and display the acknowledgment from the server
+    memset(buffer, 0, sizeof(buffer));
+    
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    printf("%s", buffer);
 
-    // Send username and password to the server
-    send(client_socket, username, sizeof(username), 0);
-    send(client_socket, password, sizeof(password), 0);
+    // Wait for 1 second
+    sleep(1);
+    //Recieve enter username propmt
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    printf("%s", buffer);
 
-    // Receive the role from the server
-    recv(client_socket, &role, sizeof(int), 0);
-
-    // Check if authentication was successful
-    if (role != -1) {
-        switch (role) {
-            case 1:
-                // Admin menu options
-                printf("Login Successful!\n");
-                printf("Welcome, admin!\n");
-                add_student(client_socket); 
-                // Add admin menu options here
-                break;
-            case 2:
-                // Professor menu options
-                printf("Login Successful!\n");
-                printf("Welcome, professor!\n");
-                // Add professor menu options here
-                break;
-            case 3:
-                // Student menu options
-                printf("Login Successful!\n");
-                printf("Welcome, student!\n");
-                // Add student menu options here
-                break;
-            default:
-                printf("Unknown role received from the server.\n");
-        }
-    } else {
-        printf("Authentication failed. Please check your username and password.\n");
-    }
-
-    // Close the client socket
-    close(client_socket);
+    
+    close(clientSocket);
 
     return 0;
 }
