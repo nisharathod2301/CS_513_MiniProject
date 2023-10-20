f8243cbecb58fcb3ed6f82925e331cddbde10255
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2023-10-20 15:57:58 +0530

    Add files via upload

---
+++ server_functions.h
@@ -0,0 +1,11 @@
+// server_functions.h
+
+#ifndef SERVER_FUNCTIONS_H
+#define SERVER_FUNCTIONS_H
+
+void addLoginDetails(const char* username, const char* password, int role);
+
+// Add other function declarations as needed
+
+#endif
+
